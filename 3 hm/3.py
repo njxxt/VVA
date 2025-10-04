34e6a1622a1740600b60180ddf7cf01f1652e3b6
njxxt <vovchenko.vladimir@phystech.edu>
2025-10-04 11:58:51 +0300

    Update 3.py

--- 3 hm/3.py
+++ 3 hm/3.py
@@ -4,8 +4,8 @@ plt.figure(figsize=(14, 10))
 
 countries = ['USSR/Ru', 'USA', 'Chi', 'Eu', 'In', 'Ja', 'Ano']
 programs = [45, 38, 22, 18, 12, 10, 15]
-
-plt.pie(programs, labels=countries, autopct='%1.f%%')
+plt.gcf().set_facecolor('#8B0000')
+plt.pie(programs, labels=countries, autopct='%1.f%%', textprops={'fontsize': 13, 'weight': 'bold', 'color': 'white'})
 
 plt.title('Spaces programs of counties', fontsize=16, fontweight='bold')
 
