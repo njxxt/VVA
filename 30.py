2a241330b7f845bd6960927400e9018fef92c85e
njxxt <vovchenko.vladimir@phystech.edu>
2025-10-17 04:05:11 +0300

    add task

---
+++ 30.py
@@ -0,0 +1,38 @@
+import re
+
+f = open(r'C:\Users\Vladi\Downloads\forTEST.txt', 'r+')
+
+
+zum = re.findall(r'\d+', f.read())
+may_count = int(zum[-1]) if zum else 0
+
+
+def deco(func):
+    def wrap(x, y):
+        global may_count
+        may_count += 1
+
+        f.seek(0)
+        f.truncate()
+
+        f.write(str(may_count))
+        return print(f"res is {func(x, y)}")
+
+    return wrap
+
+@deco
+def teso(x, y):
+    return x + y
+
+
+if input("enter the code for satrt the nuclear war: ") != 'hare emae':
+    x = int(input("x = "))
+    y = int(input("y = "))
+    teso(x, y)
+else:
+    f.seek(0)
+    f.truncate()
+
+print(f"total strats: {may_count}")
+
+f.close()
